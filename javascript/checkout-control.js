cc19da01a75f8a25aaf4eb61504b5ecf9504254a
Talita <talita@gerencianet.com.br>
2014-10-20 14:56:49 -0200

    Ajusta redirecionamento.

--- javascript/checkout-control.js
+++ javascript/checkout-control.js
@@ -1,5 +1,5 @@
 function confirm(respXml) {
-    if (respXml.status == 2) {
+    if (JSON.parse(respXml).status == 2) {
         window.location = "./sucesso";
     } else {
         window.location = "./erro-no-pagamento";
